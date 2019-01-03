6e6f282271cfd4ab45472c54fb8cbdfe0d769257
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2019-01-03 20:50:35 -0300

    Fix overflow, slight refactor

--- lib/login.dart
+++ lib/login.dart
@@ -12,24 +12,30 @@ class LoginScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(
+        child: ListView(
           children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(75.0),
-                color: Colors.white,
-              ),
-              height: 150,
-              width: 150,
-              child: Center(
-                child: Image(
-                  image: AssetImage('assets/kelvin-logo.png'),
-                  height: 80,
-                ),
-              ),
-            ),
+            _logoHeader(),
             InputForm(),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _logoHeader() {
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(75.0),
+          color: Colors.white,
+        ),
+        height: 150,
+        width: 150,
+        child: Center(
+          child: Image(
+            image: AssetImage('assets/kelvin-logo.png'),
+            height: 80,
+          ),
         ),
       ),
     );
@@ -45,58 +51,83 @@ class InputForm extends StatefulWidget {
 
 class InputFormState extends State<InputForm> {
   final _formKey = GlobalKey<FormState>();
-  LoginInfo loginInfo = LoginInfo.empty();
+  final _usernameFocus = FocusNode();
+  final _passwordFocus = FocusNode();
+  final loginInfo = LoginInfo.empty();
 
   @override
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
+      autovalidate: true,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'username',
-            ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-            },
-            onSaved: (username) {
-              loginInfo.username = username;
-            },
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'password'),
-            obscureText: true,
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-            },
-            onSaved: (password) {
-              loginInfo.password = password;
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: RaisedButton(
-              color: Colors.blue,
-              textColor: Colors.white,
-              splashColor: Colors.blueGrey,
-              onPressed: () {
-                if (_formKey.currentState.validate()) {
-                  _formKey.currentState.save();
-                  Scaffold.of(context).showSnackBar(SnackBar(
-                      content: Text(
-                          '${loginInfo.username} / ${loginInfo.password}')));
-                }
-              },
-              child: Text('Login'),
-            ),
-          ),
+          _usernameField(),
+          _passwordField(),
+          _submitButton(),
         ],
+      ),
+    );
+  }
+
+  TextFormField _usernameField() {
+    return TextFormField(
+      textInputAction: TextInputAction.next,
+      focusNode: _usernameFocus,
+      decoration: InputDecoration(
+        labelText: 'username',
+      ),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Please enter some text';
+        }
+      },
+      onFieldSubmitted: (term) {
+        _usernameFocus.unfocus();
+        FocusScope.of(context).requestFocus(_passwordFocus);
+      },
+      onSaved: (username) {
+        loginInfo.username = username;
+      },
+    );
+  }
+
+  TextFormField _passwordField() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'password'),
+      obscureText: true,
+      focusNode: _passwordFocus,
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Please enter some text';
+        }
+      },
+      onFieldSubmitted: (term) {
+        _passwordFocus.unfocus();
+      },
+      onSaved: (password) {
+        loginInfo.password = password;
+      },
+    );
+  }
+
+  Widget _submitButton() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: RaisedButton(
+        color: Colors.blue,
+        textColor: Colors.white,
+        splashColor: Colors.blueGrey,
+        onPressed: () {
+          if (_formKey.currentState.validate()) {
+            _formKey.currentState.save();
+            Scaffold.of(context).showSnackBar(SnackBar(
+                content:
+                    Text('${loginInfo.username} / ${loginInfo.password}')));
+          }
+        },
+        child: Text('Login'),
       ),
     );
   }
